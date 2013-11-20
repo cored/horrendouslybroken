879d652c18bdb45d182f4f08664c765601892374
Rafael George <george.rafael@gmail.com>
2013-11-20 09:58:02 -0400

    Fix validations for description, name presence and lenght

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,3 +1,6 @@
 class Bucket < ActiveRecord::Base
   has_many :posts
+  validates :description, length:{ maximum:1500 }
+  validates :name, length:{ maximum:64 }
+  validates :name, presence:true
 end
