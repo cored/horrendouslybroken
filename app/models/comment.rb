aa4b2b6b456c8982d0dc4419ae15ad41c2b79935
Rafael George <george.rafael@gmail.com>
2013-11-20 10:58:07 -0400

    Fix spec for validating comment's body length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
 
-  validates :body, presence:true 
+  validates :body, presence:true, 
+                   length: { maximum: 1500 }
 
   belongs_to :post 
   belongs_to :user
