39f763d4719d4291a11dde909920c71310cca320
Rafael George <george.rafael@gmail.com>
2013-11-20 11:09:53 -0400

    Fix Post validation for description presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   validates :name, presence: true
-  validates :url, format: {with: URI::regexp(%w(http https))}, allow_blank: true
+  validates :url, format: {with: URI::regexp(%w(http https))}, 
+                  allow_blank: true
+  validates :description, presence:true
+
   belongs_to :bucket
   has_many :comments, as: :commentable
 end
