62e6afbf1f29992083be0e144877b9cf12786322
Rafael George <george.rafael@gmail.com>
2013-11-20 11:11:13 -0400

    Fix validation of presence of Bucket for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :url, format: {with: URI::regexp(%w(http https))}, 
                   allow_blank: true
   validates :description, presence:true
+  validates :bucket, presence:true
 
   belongs_to :bucket
   has_many :comments, as: :commentable
